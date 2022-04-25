6b9982e34da4abae2fa49dd0a2c41b9431e16331
Melanie Schmotz <melanie.schmotz@web.de>
2022-04-25 14:41:51 +0200

    connected to database

--- hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcryptjs')
-
-const password = '123456'
-
-const salt = '$2a$10$/vMe7cFirPuxxv7Ie98sJu'
-console.log(salt)
-
-const hash = bcrypt.hashSync(password, salt)
-console.log(hash === '$2a$10$/vMe7cFirPuxxv7Ie98sJu.8d/KnKRIJohgdynKE/O3uaDnx8NMeS')
